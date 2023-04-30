a4d2d0114bc26a49dedc6844ccabbfe8c4782311
bluesolar1999 <blue.solar1999@gmail.com>
2023-04-30 09:43:10 +0900

    ✨ 「Who to follow」実装

--- src/components/layout/FollowBar.tsx
+++ src/components/layout/FollowBar.tsx
@@ -1,10 +1,27 @@
+import Avatar from "@/src/components/Avatar";
+import useUsers from "@/src/hooks/useUsers";
+
 const FollowBar = () => {
+  const { data: users = [] } = useUsers();
+
+  if (users.lenth === 0) {
+    return null;
+  }
+
   return (
     <div className="hidden px-6 py-4 lg:block">
       <div className="rounded-xl bg-neutral-800 p-4">
         <h2 className="text-xl font-semibold text-white">Who to follow</h2>
-        <div className="flex flex-col gap-6 mt-4">
-          {/* TODO USER LIST */}
+        <div className="mt-4 flex flex-col gap-6">
+          {users.map((user: Record<string, any>) => (
+            <div key={user.id} className="flex flex-row gap-4">
+              <Avatar userId={user.id} />
+              <div className="flex flex-col">
+                <p className="text-sm font-semibold text-white">{user.name}</p>
+                <p className="text-sm text-neutral-400">@{user.username}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
